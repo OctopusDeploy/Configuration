571dc619b564769f04cd7415616967c19f0c21da
Rob <robert.erez@gmail.com>
2018-10-09 15:22:13 +1000

    Remove `DataProtectionLevel` from `IKeyValueStore` as this is not available on linux

--- source/Octopus.Configuration/IKeyValueStore.cs
+++ source/Octopus.Configuration/IKeyValueStore.cs
@@ -1,13 +1,12 @@
-using System.Security.Cryptography;
 
 namespace Octopus.Configuration
 {
     public interface IKeyValueStore
     {
-        string Get(string name, DataProtectionScope? protectionScope = null);
-        TData Get<TData>(string name, TData defaultValue = default(TData), DataProtectionScope? protectionScope = null);
-        void Set(string name, string value, DataProtectionScope? protectionScope = null);
-        void Set<TData>(string name, TData value, DataProtectionScope? protectionScope = null);
+        string Get(string name, bool? machineKeyEncrypted = false);
+        TData Get<TData>(string name, TData defaultValue = default(TData), bool? machineKeyEncrypted = false);
+        void Set(string name, string value, bool? machineKeyEncrypted = false);
+        void Set<TData>(string name, TData value, bool? machineKeyEncrypted = false);
 
         void Remove(string name);
 
