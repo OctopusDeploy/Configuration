9124ede8cf8bf0527d3f409a36283ee50fceb7bf
Rob <robert.erez@gmail.com>
2018-10-11 09:11:41 +1000

    Rename property

--- source/Octopus.Configuration/IKeyValueStore.cs
+++ source/Octopus.Configuration/IKeyValueStore.cs
@@ -3,11 +3,11 @@ namespace Octopus.Configuration
 {
     public interface IKeyValueStore
     {
-        string Get(string name, ProtectionLevel machineKeyEncrypted = ProtectionLevel.None);
-        TData Get<TData>(string name, TData defaultValue = default(TData), ProtectionLevel machineKeyEncrypted  = ProtectionLevel.None);
+        string Get(string name, ProtectionLevel protectionLevel = ProtectionLevel.None);
+        TData Get<TData>(string name, TData defaultValue = default(TData), ProtectionLevel protectionLevel  = ProtectionLevel.None);
         
-        void Set(string name, string value, ProtectionLevel machineKeyEncrypted  = ProtectionLevel.None);
-        void Set<TData>(string name, TData value, ProtectionLevel machineKeyEncrypted  = ProtectionLevel.None);
+        void Set(string name, string value, ProtectionLevel protectionLevel  = ProtectionLevel.None);
+        void Set<TData>(string name, TData value, ProtectionLevel protectionLevel  = ProtectionLevel.None);
 
         void Remove(string name);
 
