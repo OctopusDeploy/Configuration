22d2a446aef539afdc649e4bc36614094374ccaf
Rob <robert.erez@gmail.com>
2018-10-10 13:27:51 +1000

    Make `machineKeyEncrypted` non nullable

--- source/Octopus.Configuration/IKeyValueStore.cs
+++ source/Octopus.Configuration/IKeyValueStore.cs
@@ -3,10 +3,10 @@ namespace Octopus.Configuration
 {
     public interface IKeyValueStore
     {
-        string Get(string name, bool? machineKeyEncrypted = false);
-        TData Get<TData>(string name, TData defaultValue = default(TData), bool? machineKeyEncrypted = false);
-        void Set(string name, string value, bool? machineKeyEncrypted = false);
-        void Set<TData>(string name, TData value, bool? machineKeyEncrypted = false);
+        string Get(string name, bool machineKeyEncrypted = false);
+        TData Get<TData>(string name, TData defaultValue = default(TData), bool machineKeyEncrypted = false);
+        void Set(string name, string value, bool machineKeyEncrypted = false);
+        void Set<TData>(string name, TData value, bool machineKeyEncrypted = false);
 
         void Remove(string name);
 
