8bcd9b175881847bc0b05951d79d0a601848186c
slewis74 <slewis74@gmail.com>
2020-09-11 09:39:15 +1000

    splitting the keyvaluestore made many things complicated in server, changing the interface to return whether updates are supported. So calls to Set etc will return false rather than throw and then the caller can decide what to do

--- source/Octopus.Configuration/IKeyValueStore.cs
+++ source/Octopus.Configuration/IKeyValueStore.cs
@@ -6,7 +6,37 @@ namespace Octopus.Configuration
     {
         string? Get(string name, ProtectionLevel protectionLevel = ProtectionLevel.None);
 
-        // ReSharper disable once RedundantTypeSpecificationInDefaultExpression
-        TData Get<TData>(string name, TData defaultValue = default(TData), ProtectionLevel protectionLevel = ProtectionLevel.None);
+        TData Get<TData>(string name, TData defaultValue = default, ProtectionLevel protectionLevel = ProtectionLevel.None);
+        
+        /// <summary>
+        /// Sets a configuration value, if this is supported by the startup strategy  
+        /// </summary>
+        /// <param name="name">The setting's name</param>
+        /// <param name="value">The setting's value</param>
+        /// <param name="protectionLevel">The protection level of the value, <see cref="ProtectionLevel"/></param>
+        /// <returns>True if the value was updated, or false if updates are not supported.</returns>
+        bool Set(string name, string? value, ProtectionLevel protectionLevel = ProtectionLevel.None);
+
+        /// <summary>
+        /// Sets a configuration value, if this is supported by the startup strategy  
+        /// </summary>
+        /// <param name="name">The setting's name</param>
+        /// <param name="value">The setting's value</param>
+        /// <param name="protectionLevel">The protection level of the value, <see cref="ProtectionLevel"/></param>
+        /// <returns>True if the value was updated, or false if updates are not supported.</returns>
+        bool Set<TData>(string name, TData value, ProtectionLevel protectionLevel = ProtectionLevel.None);
+
+        /// <summary>
+        /// Removes a configuration value, if this is supported by the startup strategy  
+        /// </summary>
+        /// <param name="name">The setting's name</param>
+        /// <returns>True if the value was removed, or false if updates are not supported.</returns>
+        bool Remove(string name);
+
+        /// <summary>
+        /// Saves the configuration, if this is supported by the startup strategy  
+        /// </summary>
+        /// <returns>True if the configuration was saved, or false if updates are not supported.</returns>
+        bool Save();
     }
 }
